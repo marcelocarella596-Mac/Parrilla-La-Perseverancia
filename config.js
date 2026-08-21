26bcb6584603bc3f7b0d0d62ea1f01f0f3203ecb
marcelocarella596-Mac <marcelocarella596@gmail.com>
2026-08-20 23:01:30 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
-// config.js - CONFIG REAL OPERATIVA
 const firebaseConfig = {
-  apiKey: "AIzaSyAvVFprDSOdog587jJ3V74u-HnJnPFCyLGU",
-  authDomain: "parrilla-la-perseverancia-f3f54.firebaseapp.com",
-  databaseURL: "https://parrilla-la-perseverancia-f3f54-default-rtdb.firebaseio.com",
-  projectId: "parrilla-la-perseverancia-f3f54",
-  storageBucket: "parrilla-la-perseverancia-f3f54.firebasestorage.app",
-  messagingSenderId: "899082319184",
-  appId: "1:899082319184:web:9876161963706786fe856"
+  apiKey: "AIzaSyB5uz0yQz9Y2m9yKK-3y_tE2yq1ZQcF7g8",
+  authDomain: "parrilla-la-perseveranci-f3f54.firebaseapp.com",
+  databaseURL: "https://parrilla-la-perseveranci-f3f54-default-rtdb.firebaseio.com",
+  projectId: "parrilla-la-perseveranci-f3f54",
+  storageBucket: "parrilla-la-perseveranci-f3f54.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abc123"
 };
